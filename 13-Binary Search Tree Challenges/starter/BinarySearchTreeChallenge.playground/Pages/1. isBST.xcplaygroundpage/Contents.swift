d194376ebca1ad5fb26987941f23e2e1c2d213c3
Frank.zhang <894918941@qq.com>
2022-03-17 00:37:18 +0800

    BinarySearchTreeChallenge

---
+++ 13-Binary Search Tree Challenges/starter/BinarySearchTreeChallenge.playground/Pages/1. isBST.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: [Previous](@previous)
+/*:
+ # Binary Search Tree Challenges
+ ## 1. Binary Tree or Binary Search Tree?
+ Create a function that checks if a binary tree is a binary search tree.
+ */
+var bst = BinarySearchTree<Int>()
+bst.insert(3)
+bst.insert(1)
+bst.insert(4)
+bst.insert(0)
+bst.insert(2)
+bst.insert(5)
+print(bst)
+//: [Next](@next)
---
+++ 13-Binary Search Tree Challenges/starter/BinarySearchTreeChallenge.playground/Sources/Helpers.swift
@@ -0,0 +1,5 @@
+public func examples(of description: String, action: () -> Void) {
+    print("---Example of: \(description)---")
+    action()
+    print()
+}
