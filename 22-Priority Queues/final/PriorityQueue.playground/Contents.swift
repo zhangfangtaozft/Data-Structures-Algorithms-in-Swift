6319288ea4d55c6f4d12ce0a89f46e3a6e325671
Frank.zhang <894918941@qq.com>
2022-03-24 02:31:47 +0800

    Add Priority Queue

---
+++ 22-Priority Queues/final/PriorityQueue.playground/Contents.swift
@@ -0,0 +1,30 @@
+struct PriorityQueue<Element: Equatable>: Queue {
+   
+    private var heap: Heap<Element>
+    
+    init(sort: @escaping (Element, Element) -> Bool, elements: [Element] = []) {
+      heap = Heap(sort: sort, elements: elements)
+    }
+    
+    var isEmpty: Bool {
+        heap.isEmpty
+    }
+    
+    var peek: Element?{
+        heap.peek()
+    }
+    
+    mutating func enqueue(_ element: Element) -> Bool {
+        heap.insert(element)
+        return true
+    }
+    
+    mutating func dequeue() -> Element? {
+        heap.remove()
+    }
+}
+
+var priorityQueue = PriorityQueue(sort: >, elements: [1,12,3,4,1,6,8,7])
+while !priorityQueue.isEmpty {
+    print(priorityQueue.dequeue()!)
+}
